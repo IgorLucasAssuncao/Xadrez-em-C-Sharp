91231ac1779ab4b2737cc1103a2a9428f71c403b
Igor Lucas  <igorlucas1217@gmail.com>
2023-12-18 13:42:56 -0300

    Implementando o tabuleiro

---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QtdMovimentos = 0;
+            this.tab = tab;
+        }
+
+    }
+}
---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int Coluna { get; set; }
+        public int Linha { get; set; }
+        private Peca[,] Pecas;
+
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            Coluna = coluna;
+            Linha = linha;
+            Pecas = new Peca[Linha, Coluna];
+        }
+    }
+}
